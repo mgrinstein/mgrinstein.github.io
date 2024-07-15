ab3214c01ee66c370a67ab6e8e728df1d8003084
Monique Grinstein <monique.grinstein@lego.com>
2024-07-15 12:48:04 +0100

    refactor: delete link

--- src/config.ts
+++ src/config.ts
@@ -47,13 +47,13 @@ export const profileConfig: ProfileConfig = {
   name: "Monique Grinstein",
   bio: "Blog draft here.",
   links: [
-    {
-      name: "Twitter",
-      icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-      // You will need to install the corresponding icon set if it's not already included
-      // `pnpm add @iconify-json/<icon-set-name>`
-      url: "https://twitter.com"
-    },
+    // {
+    //   name: "Twitter",
+    //   icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+    //   // You will need to install the corresponding icon set if it's not already included
+    //   // `pnpm add @iconify-json/<icon-set-name>`
+    //   url: "https://twitter.com"
+    // },
     {
       name: "Steam",
       icon: "fa6-brands:steam",
