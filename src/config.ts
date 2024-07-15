55855d06778db5e27a8a262c721aeaa8edba1a4b
Monique Grinstein <monique.grinstein@lego.com>
2024-07-15 12:29:47 +0100

    refactor: bio

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: "Monique Grinstein",
-  bio: "Blog draft.",
+  bio: "Blog draft here.",
   links: [
     {
       name: "Twitter",
