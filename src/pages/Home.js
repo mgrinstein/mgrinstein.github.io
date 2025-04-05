5062e1a68b50a8c300589b4f34d383c195f20cbe
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2025-04-05 10:08:58 +0100

    refactor: simplify home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,12 @@ export function Home() {
             <WebsiteNavbar />
             <div className="PageContent">
                 <div className="intro-text">
-                    <HomeComponent image={images.myPhoto2024} title={<h2>&gt; &nbsp;Hello, World!</h2>} text={                        <>
-                            I am a Software Engineer specialized in backend services and cloud technologies. Deeply committed to technical excellence and continuous learning.
-                            <br /><br />Beyond my professional work, I am an active speaker and community builder within the AWS ecosystem. Focus areas include Software Architecture, AI, Infrastructure as Code, and emerging technologies.
-                            <br /><br />Interested in broadening my contributions to open-source projects, particularly for Satellite and Space applications.
-                        </>}
-                         />
+                    <HomeComponent image={images.myPhoto2024} title={<h2>&gt; &nbsp;Hello, World!</h2>} text={<>
+                        I am a Software Engineer specialized in backend services and cloud technologies. Deeply committed to technical excellence and continuous learning.
+                        <br /><br />Beyond my professional work, I am an active speaker and community builder within the AWS ecosystem. Focus areas include Software Architecture, AI, Infrastructure as Code, and emerging technologies.
+                        <br /><br />Interested in broadening my contributions to open-source projects, particularly for Satellite and Space applications.
+                    </>}
+                    />
                 </div>
             </div>{" "}
         </div>
