7f02f2283c0f93d1e15e180cf494695d44941ab3
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2025-04-05 10:08:39 +0100

    refactor: simplify home component

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,21 +1,12 @@
 import React from "react";
 import "./HomeComponent.css";
 
-const HomeComponentText = ({ text }) => {
-
-    return <p>{text}</p>
-
-};
-
 export function HomeComponent({ title, image, text }) {
     return (
         <div className="home-component-container">
             <div className="home-component-text">
                 {title && <h2>&gt; &nbsp;Hello, World!</h2>}
-                <HomeComponentText
-                    text={text
-                    }
-                />
+                <p>{text}</p> 
             </div>
             {image && (
 
