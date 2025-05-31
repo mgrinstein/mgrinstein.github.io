5ea855c9f46e4b9ffc605adbd0520b0659ae87fb
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2025-05-31 12:29:35 +0100

    feat: add hdf4 processor project component

--- src/images/projects/index.js
+++ src/images/projects/index.js
@@ -2,5 +2,6 @@ import settle from './settle.png';
 import volcano from './volcano.png';
 import website from './website.png';
 import groundster from './groundster.png';
+import hdf4 from './hdf4.png';
 
-export { settle, volcano, website, groundster };
+export { settle, volcano, website, groundster, hdf4 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,48 +6,56 @@ export function Projects() {
     <div className="PageContainer">
       <WebsiteNavbar />
       <div className="PageContent">
-      <div className="intro-text">
-        <p>
-        Here I showcase projects I have worked on recently.<br/> Other projects, contributions, and stats can be found on <a href="https://github.com/mgrinstein" target="_blank">my GitHub page</a>. <br />
-        </p>
-      </div>
+        <div className="intro-text">
+          <p>
+            Here I showcase projects I have worked on recently.<br /> Other projects, contributions, and stats can be found on <a href="https://github.com/mgrinstein" target="_blank">my GitHub page</a>. <br />
+          </p>
+        </div>
 
-      <div className="content-grid">
-        <ContentElement
-          title="SETTLE - Satellite Element Tracking via TLE"
-          description="A satellite tracker using Celestrak TLE data with SGP4 orbit propagation and interactive map visualisations"
-          image={images.settle}
-          link="https://github.com/mgrinstein/SETTLE"
-          hashtags={["Python", "CelesTrak", "sgp4"]}
-          buttonText="Source Code"
-        />
-        <ContentElement
-          title="GROUNDSTER - GROUND Station TElemetry Receiver"
-          description="A simulated ground station that receives telemetry data from a satellite via TCP socket connection"
-          image={images.groundster}
-          link="https://github.com/mgrinstein/GROUNDSTER"
-          hashtags={["C++", "Python", "FASTAPI"]}
-          buttonText="Source Code"
-        />
-        <ContentElement
-          title=" KILAUEA-EO - Kīlauea Earth Observation"
-          description="An analysis of a recent volcanic eruption using Sentinel-2 imagery in the NIR band from Google Earth Engine"
-          image={images.volcano}
-          link="https://github.com/mgrinstein/volcano-image-processing"
-          hashtags={["Python", "JupyterNotebook", "GeoTIFF", "EarthObservation"]}
-          buttonText="Source Code"
-        />
-        <ContentElement
-          title="Portfolio Website"
-          description="Déjà vu? Building this webpage"
-          image={images.website}
-          link="https://github.com/mgrinstein/mgrinstein.github.io"
-          hashtags={["JavaScript", "React", "GitHubActions", "CustomDomain"]}
-          buttonText="Source Code"
+        <div className="content-grid">
+          <ContentElement
+            title="SETTLE - Satellite Element Tracking via TLE"
+            description="A satellite tracker using Celestrak TLE data with SGP4 orbit propagation and interactive map visualisations"
+            image={images.settle}
+            link="https://github.com/mgrinstein/SETTLE"
+            hashtags={["Python", "CelesTrak", "sgp4"]}
+            buttonText="Source Code"
+          />
+          <ContentElement
+            title="GROUNDSTER - GROUND Station TElemetry Receiver"
+            description="A simulated ground station that receives telemetry data from a satellite via TCP socket connection"
+            image={images.groundster}
+            link="https://github.com/mgrinstein/GROUNDSTER"
+            hashtags={["C++", "Python", "FASTAPI"]}
+            buttonText="Source Code"
+          />
+          <ContentElement
+            title="KILAUEA-EO - Kīlauea Earth Observation"
+            description="An analysis of a recent volcanic eruption using Sentinel-2 imagery in the NIR band from Google Earth Engine"
+            image={images.volcano}
+            link="https://github.com/mgrinstein/KILAUEA-EO"
+            hashtags={["Python", "JupyterNotebook", "GeoTIFF", "EarthObservation"]}
+            buttonText="Source Code"
+          />
+          <ContentElement
+            title="HDF4 Processor"
+            description="A tool to parse, process, and visualise satellite data stored in HDF4 format"
+            image={images.hdf4}
+            link="https://github.com/mgrinstein/HDF4_processor"
+            hashtags={["Python", "HDF4", "cartopy", "EarthObservation"]}
+            buttonText="Source Code"
+          />
+          <ContentElement
+            title="Portfolio Website"
+            description="Déjà vu? Building this webpage"
+            image={images.website}
+            link="https://github.com/mgrinstein/mgrinstein.github.io"
+            hashtags={["JavaScript", "React", "GitHubActions", "CustomDomain"]}
+            buttonText="Source Code"
 
-        />
+          />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
