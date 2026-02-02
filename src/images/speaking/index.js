5c97af27b57f365d0be01fb4f2b77877986ae277
Monique Grinstein <moniquecalmon@poli.ufrj.br>
2026-02-02 11:31:57 +0000

    docs: add entry for UNOOSA

--- src/images/speaking/index.js
+++ src/images/speaking/index.js
@@ -1,10 +1,21 @@
-import talkcdk from './talk-cdk.jpeg';
-import reinvent from './reinvent.jpeg';
-import aihackathon from './AI-hackathon.jpg';
-import aihackathon2 from './AI-hackathon2.jpg';
-import devcon from './devcon.png';
-import devcon2 from './devcon2.png';
-import reinvent2 from './reinvent2.jpeg';
-import lunar from './lunar.png';
+import talkcdk from "./talk-cdk.jpeg";
+import reinvent from "./reinvent.jpeg";
+import aihackathon from "./AI-hackathon.jpg";
+import aihackathon2 from "./AI-hackathon2.jpg";
+import devcon from "./devcon.png";
+import devcon2 from "./devcon2.png";
+import reinvent2 from "./reinvent2.jpeg";
+import lunar from "./lunar.png";
+import unoosa from "./unoosa.jpg";
 
-export { talkcdk, reinvent, reinvent2, aihackathon, aihackathon2, devcon, devcon2, lunar };
+export {
+  talkcdk,
+  reinvent,
+  reinvent2,
+  aihackathon,
+  aihackathon2,
+  devcon,
+  devcon2,
+  lunar,
+  unoosa,
+};
--- src/pages/Speaking.js
+++ src/pages/Speaking.js
@@ -2,102 +2,123 @@ import { ContentElement, WebsiteNavbar, Footer } from "../components";
 import * as images from "../images/speaking";
 
 export function Speaking() {
-    return (
-        <div className="PageContainer">
-            <WebsiteNavbar />
-            <div className="PageContent">
-                <div className="intro-text">
-                    <p>
-                        I am passionate about sharing knowledge with the developer community
-                        and have had the opportunity to speak at local and international
-                        events in the past year:
-                    </p>
-                </div>
-
-                <div className="content-grid">
-                                        <ContentElement
-                        title={
-                            <>
-                                Space Studies Program - International Space University
-                                <br />
-                                <i>South Korea, 21 August 2025</i>
-                            </>
-                        }
-                        description="Sustainable Lunar Infrastructure Development"
-                        image={images.lunar}
-                        hashtags={["Space", "Research"]}
-                    />
-                    <ContentElement
-                        title={
-                            <>
-                                AWS re:Invent
-                                <br />
-                                <i>Las Vegas, 02 - 06 December 2024</i>
-                            </>
-                        }
-                        description="All Builders Welcome Mentoring Sessions"
-                        image={images.reinvent2}
-                        hashtags={["AWS", "TechLeadership", "Mentoring", "CloudComputing"]}
-                    />
-                    <ContentElement
-                        title={
-                            <>
-                                AWS Cloud Day Dublin
-                                <br />
-                                <i>Dublin, 10 October 2024</i>
-                            </>
-                        }
-                        description="GenAI Made Easy for Devs - with AWS CDK Constructs"
-                        image={images.talkcdk}
-                        hashtags={["GenAI", "AWS", "CDK", "InfrastructureAsCode"]}
-                    />
-
-                    <ContentElement
-                        title={
-                            <>
-                                LEGO® DevCon
-                                <br />
-                                <i>London, 1 - 2 October 2024</i>
-                            </>
-                        }
-                        description={
-                            <>
-                                <i>Keynote:</i> Making Promotions Awesome - Our InnerSource Journey
-                            </>
-                        } image={images.devcon}
-                        hashtags={["OpenSource", "InnerSource", "TechStorytelling"]}
-                    />
-
-<ContentElement
-                        title={
-                            <>
-                                LEGO® DevCon
-                                <br />
-                                <i>London, 1 - 2 October 2024</i>
-                            </>
-                        }
-                        description={
-                            <>
-                                <i>Creators Track:</i> Making Tech Presentations Memorable - A Guide to Structured Storytelling
-                            </>
-                        } image={images.devcon2}
-                        hashtags={["OpenSource", "InnerSource", "TechStorytelling"]}
-                    />
-                    <ContentElement
-                        title={
-                            <>
-                                AI Hackathon by Encode Club
-                                <br />
-                                <i>London, 9 March 2024</i>
-                            </>
-                        }
-                        description="Pitch Perfect: Crafting Success Through Storytelling"
-                        image={images.aihackathon2}
-                        hashtags={["TechStorytelling", "DemoPitch", "GenAISolutions",]}
-                    />
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div className="PageContainer">
+      <WebsiteNavbar />
+      <div className="PageContent">
+        <div className="intro-text">
+          <p>
+            I am passionate about sharing knowledge with the developer community
+            and have had the opportunity to speak at local and international
+            events in the past year:
+          </p>
         </div>
-    );
+
+        <div className="content-grid">
+          <ContentElement
+            title={
+              <>
+                United Nations Office for Outer Space Affairs (UNOOSA)
+                <br />
+                <i>Vienna, 18 - 19 November 2025</i>
+              </>
+            }
+            description="Contributing Technical and AI Perspectives to Future Commercial Lunar Policy"
+            image={images.unoosa}
+            hashtags={[
+              "SpacePolicy",
+              "TechToPolicy",
+              "AIinPolicy",
+              "LunarEconomy",
+            ]}
+          />
+          <ContentElement
+            title={
+              <>
+                Space Studies Program - International Space University
+                <br />
+                <i>South Korea, 21 August 2025</i>
+              </>
+            }
+            description="Sustainable Lunar Infrastructure Development"
+            image={images.lunar}
+            hashtags={["Space", "Research"]}
+          />
+          <ContentElement
+            title={
+              <>
+                AWS re:Invent
+                <br />
+                <i>Las Vegas, 02 - 06 December 2024</i>
+              </>
+            }
+            description="All Builders Welcome Mentoring Sessions"
+            image={images.reinvent2}
+            hashtags={["AWS", "TechLeadership", "Mentoring", "CloudComputing"]}
+          />
+          <ContentElement
+            title={
+              <>
+                AWS Cloud Day Dublin
+                <br />
+                <i>Dublin, 10 October 2024</i>
+              </>
+            }
+            description="GenAI Made Easy for Devs - with AWS CDK Constructs"
+            image={images.talkcdk}
+            hashtags={["GenAI", "AWS", "CDK", "InfrastructureAsCode"]}
+          />
+
+          <ContentElement
+            title={
+              <>
+                LEGO® DevCon
+                <br />
+                <i>London, 1 - 2 October 2024</i>
+              </>
+            }
+            description={
+              <>
+                <i>Keynote:</i> Making Promotions Awesome - Our InnerSource
+                Journey
+              </>
+            }
+            image={images.devcon}
+            hashtags={["OpenSource", "InnerSource", "TechStorytelling"]}
+          />
+
+          <ContentElement
+            title={
+              <>
+                LEGO® DevCon
+                <br />
+                <i>London, 1 - 2 October 2024</i>
+              </>
+            }
+            description={
+              <>
+                <i>Creators Track:</i> Making Tech Presentations Memorable - A
+                Guide to Structured Storytelling
+              </>
+            }
+            image={images.devcon2}
+            hashtags={["OpenSource", "InnerSource", "TechStorytelling"]}
+          />
+          <ContentElement
+            title={
+              <>
+                AI Hackathon by Encode Club
+                <br />
+                <i>London, 9 March 2024</i>
+              </>
+            }
+            description="Pitch Perfect: Crafting Success Through Storytelling"
+            image={images.aihackathon2}
+            hashtags={["TechStorytelling", "DemoPitch", "GenAISolutions"]}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
